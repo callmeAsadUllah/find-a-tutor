291f69ab3d842305bf0f96ea84d6586d80eee0f7
Muhammad Asad Ullah <muhammad.asadullah12122002@gmail.com>
2024-12-04 13:30:36 +0500

    configuration updated

---
+++ src/modules/pusher/pusher.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('pusher')
+export class PusherController {}
--- src/modules/pusher/pusher.module.ts
+++ src/modules/pusher/pusher.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PusherController } from './pusher.controller';
+import { PusherService } from './pusher.service';
 
-@Module({})
+@Module({
+  imports: [],
+  providers: [PusherService],
+  controllers: [PusherController],
+  exports: [],
+})
 export class PusherModule {}
