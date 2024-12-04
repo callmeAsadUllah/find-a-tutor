bb4d3724ae31fadc38caadd26cbfe81bc7281495
Muhammad Asad Ullah <muhammad.asadullah12122002@gmail.com>
2024-12-04 15:15:59 +0500

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('find A Tutor')
+    .setDescription('find A Tutor API description')
+    .setVersion('1.0')
+    .addTag('tutor')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(port);
 }
 bootstrap();
