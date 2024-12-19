c700f3dabcbb95d6dc97bb66b6d0fad9de6d64a4
callmeAsadUllah <mumayr12@gmail.com>
2024-12-19 17:22:48 +0500

    code updated.

--- src/modules/comments/dtos/comment.dto.ts
+++ src/modules/comments/dtos/comment.dto.ts
@@ -3,23 +3,18 @@ import {
   IsString,
   IsOptional,
   IsArray,
-  IsDate,
   IsMongoId,
 } from 'class-validator';
 import { Types } from 'mongoose';
 
 export class CreateCommentDto {
-  @IsMongoId()
   @IsNotEmpty()
+  @IsMongoId()
   userId: Types.ObjectId;
 
-  @IsString()
   @IsNotEmpty()
+  @IsString()
   content: string;
-
-  @IsDate()
-  @IsOptional()
-  publishedDate?: Date;
 
   @IsArray()
   @IsOptional()
